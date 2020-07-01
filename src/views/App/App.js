955213cbea14ca9a06b9b136e9673cef110b855b
Hector Sanchez <hola@hectorsanchez.mx>
2020-07-01 17:50:42 -0500

    Update: implementing react context and adding first step  from wizard component

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import cx from 'classnames'
 
+import { AppProvider } from '../../context'
+
 import Hello from '../../components/Hello'
+import GettingStarted from '../../components/GettingStarted'
 
 import styles from './App.less'
 
 export default function App() {
   console.trace()
   return (
-    <section className={cx(styles.AppShell, styles.bodyText)}>
-      <section className={cx('AppMain', styles.AppMain)}>
-        <Hello />
+    <AppProvider>
+      <section className={cx(styles.AppShell, styles.bodyText)}>
+        <section className={cx('AppMain', styles.AppMain)}>
+          <GettingStarted />
+        </section>
       </section>
-    </section>
+    </AppProvider>
   )
 }
