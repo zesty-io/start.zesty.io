6dd5f8051dfd8caceefa94539a702de81bff287f
Joshua Hanson <joshua.hanson@zesty.io>
2020-07-01 21:06:34 -0500

    regenerator-runtime, core-js added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 import App from './views/App'
+
+window.CONFIG = __CONFIG__
 
 ReactDOM.render(<App />, document.getElementById('root'))
