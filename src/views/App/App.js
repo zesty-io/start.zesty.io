449435996a1e6e4af7d6aa1f070ac805ebeefa46
Nar Cuenca <narc.ph@gmail.com>
2025-10-21 07:54:28 +0800

    Update browser integration

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import cx from 'classnames'
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
+import { createBrowserHistory } from 'history'
 
 import { AppProvider } from '../../context'
 import { AppError } from './../../components/AppError'
@@ -9,15 +10,17 @@ import GettingStarted from '../../components/GettingStarted'
 
 import styles from './App.less'
 
+const history = createBrowserHistory()
+
+// FIXME: ProjectID doesn't seem to be valid anymore
 if (['stage', 'production'].includes(__CONFIG__.ENV)) {
   Sentry.init({
     release: __CONFIG__.build.data.gitCommit,
     environment: __CONFIG__.ENV,
-    integrations: [new Integrations.BrowserTracing()],
-    dsn:
-      'https://9e46029d4aef4ae2a4f31c4bd13ddc77@o162121.ingest.sentry.io/5673717',
+    integrations: [Sentry.reactRouterV5BrowserTracingIntegration({ history })],
+    dsn: 'https://9e46029d4aef4ae2a4f31c4bd13ddc77@o162121.ingest.sentry.io/5673717',
     autoSessionTracking: true,
-    tracesSampleRate: 1.0
+    tracesSampleRate: 1.0,
   })
 }
 
